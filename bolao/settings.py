4d40e154811456d79b76828c48cf1e5fa89954b5
Diego Lirio <diegolirio.dl@gmail.com>
2013-07-13 11:52:01 -0300

    Update settings.py

--- bolao/settings.py
+++ bolao/settings.py
@@ -15,12 +15,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'bolao_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        #'USER': 'admin_sys',
-        'PASSWORD': '',
+        #'USER': '',
+        'USER': 'admin_sys',
+        #'PASSWORD': '',
         'PASSWORD': 'admin_sys',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
