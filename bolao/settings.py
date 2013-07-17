6891dee34ae08bed21d0392508d158b295caa691
Diego Lirio <diegolirio.dl@gmail.com>
2013-07-17 15:29:46 -0300

    Update settings.py

--- bolao/settings.py
+++ bolao/settings.py
@@ -11,7 +11,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -37,6 +37,7 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+"""
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
@@ -183,8 +184,8 @@ EMAIL_HOST_PASSWORD = 'dyeg1986'
 EMAIL_USE_TLS = True
 # -----------------------------------------------------
 
-#import dj_database_url
-#DATABASES['default'] =  dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
  
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
