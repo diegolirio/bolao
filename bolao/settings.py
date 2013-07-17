ecce61d0e5bb59e47b38b9a44ddfacc7a9356304
Diego Lirio <diegolirio.dl@gmail.com>
2013-07-17 15:46:46 -0300

    Update settings.py

--- bolao/settings.py
+++ bolao/settings.py
@@ -186,6 +186,6 @@ EMAIL_USE_TLS = True
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
- 
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
