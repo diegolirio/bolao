361ad7e447426fc01ea7e3b898b5742708b4202c
Diego Lirio <diegolirio.dl@gmail.com>
2013-07-25 17:29:17 -0300

    ...sqlite config heroku

--- bolao/settings.py
+++ bolao/settings.py
@@ -1,6 +1,8 @@
 # Django settings for bolao project.
 import os
 
+import dj_database_url
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,6 +14,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -22,6 +25,12 @@ DATABASES = {
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
+}
+"""
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + PROJECT_DIR.child('database.db'))
 }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
@@ -169,7 +178,6 @@ EMAIL_HOST_PASSWORD = 'dyeg1986'
 EMAIL_USE_TLS = True
 # -----------------------------------------------------
 
-#import dj_database_url
 #DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
