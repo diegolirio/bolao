4e797a8d75dc7d760e5fa0d6453b1257b4d66a26
Diego Lirio <diegolirio.dl@gmail.com>
2013-09-19 14:07:17 -0300

    deploy

--- bolao/settings.py
+++ bolao/settings.py
@@ -63,7 +63,8 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(ROOT_PROJECT, 'media') 
+#MEDIA_ROOT = os.path.join(ROOT_PROJECT, 'media') 
+MEDIA_ROOT = PROJECT_DIR.child('media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -74,7 +75,8 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(ROOT_PROJECT, 'static') 
+#STATIC_ROOT = os.path.join(ROOT_PROJECT, 'static') 
+STATIC_ROOT = PROJECT_DIR.child('static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
