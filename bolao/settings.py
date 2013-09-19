d0b5a429779433ccd287b587c4ddecca0276eed9
Diego Lirio <diegolirio.dl@gmail.com>
2013-09-19 13:42:24 -0300

    edit settings

--- bolao/settings.py
+++ bolao/settings.py
@@ -1,7 +1,9 @@
 # Django settings for bolao project.
-import os
 
 #import dj_database_url
+import os
+import dj_database_url
+from unipath import Path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -14,17 +16,24 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#        'NAME': 'bolao_db',                      # Or path to database file if using sqlite3.
+#        # The following settings are not used with sqlite3:
+#        'USER': '',
+#        'PASSWORD': '',
+#        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#        'PORT': '',                      # Set to empty string for default.
+#    }
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'bolao_db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + PROJECT_DIR.child('mybolao.db'))
 }
+
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
